0400a7ce80cadd15fac9d2c00553d7c92d7bfc8e
kakapowu <kakapowu@8442cf90-862c-11de-9a5f-5115e56f9795>
2009-09-03 10:22:02 +0000

    监控管理，应用错误

--- Admin/controllers/index/monitor.class.php
+++ Admin/controllers/index/monitor.class.php
@@ -25,62 +25,17 @@ class monitor{
 		//$tpl->assign('items',$items);
 		
 	}
-	function view_stats(){
+	function view_errorlog(){
 		global $tpl;
-	
-		$tpl->assign("monitorHadle",$GLOBALS['monitor']['monitorHandle']);
-		if($GLOBALS['monitor']['monitorHandle']=='memcache'){
-			$server_slabs = array();
-			$server_slabs = $this->mMonitorObj->getOnlineStats("slabs");
-			$all_stats = $this->mMonitorObj->getOnlineStats();
-			
-			$key_arr = array();
-			foreach($server_slabs as $server=>$slabs){
-				
-				foreach ($slabs as $slab_id=>$slab_info){
-					if(is_int($slab_id)){
-						$items = $this->mMonitorObj->getOnlineStats("cachedump",$slab_id,0);
-	
-						foreach($items as $ser=>$item){
-							if(!empty($item)){						
-								foreach($item as $key=>$v){
-									if($server==$ser){
-										$life_time = $v[1] - $all_stats[$server]['time'];
-										//if($life_time>0){
-											$key_arr[$key][0] = $life_time;
-											$key_arr[$key][1] = $v[0];
-										//}
-									}
-								}
-							}
-						}
-					}		
-				}
-			
-				asort($key_arr);
-				$total_stats[$server] = $key_arr;
-				$key_arr = array();
-			}
-			$tpl->assign("all_stats",$all_stats);
-			$tpl->assign("total_stats",$total_stats);
-		}
+		if(!empty($_GET['error_no'])) $con['error_no'] = $_GET['error_no'];
+		$con['order'] = "error_no";
+
+		$items = $this->mMonitorObj->getErrorLog($con,4);
 		
-		if($GLOBALS['monitor']['monitorHandle']=='database'){
-			$all_stats = $this->mMonitorObj->getOnlineStatsDb();
-			$tpl->assign("all_stats",$all_stats);
-		}
-		
+		$tpl->assign('items',$items);
 	}
-	function view_viewmonitor(){
-		if($GLOBALS['monitor']['monitorHandle']=='memcache'){
-			$value  = $this->mMonitorObj->getMonitorByKey($_GET['server'],$_GET['key']);
-		}
-		if($GLOBALS['monitor']['monitorHandle']=='database'){
-			$value  = $this->mMonitorObj->getMonitorBySesskey($_GET['key']);
-		}
-		echo "<pre>";
-		print_r($value);
-		die;
+	function view_eventlog(){
+		global $tpl;
 	}
 	
 	
