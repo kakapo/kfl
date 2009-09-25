9e745349a7f56ff3eaaf0c50d89149ee9ebfe194
kakapowu <kakapowu@8442cf90-862c-11de-9a5f-5115e56f9795>
2009-09-25 07:54:24 +0000

    改变界面风格程序更新

--- Admin/controllers/index/index.class.php
+++ Admin/controllers/index/index.class.php
@@ -1,7 +1,12 @@
 <?php
 class index {
 	function view_login(){
-		
+		global $tpl;
+		$style = 'nihilo';
+		if(isset($_COOKIE['_GUI_STYLE'])){
+			$style = $_COOKIE['_GUI_STYLE'];
+		}
+		$tpl->assign('ui_style',$style);
 	}
 	function op_dologin(){
 		if($_POST['name']==$GLOBALS['config']['admin'] && $_POST['password']==$GLOBALS['config']['password']){
@@ -21,7 +26,14 @@ class index {
 		global $tpl;
 		$this->is_login();
 		$path = realpath(APP_DIR."/..");
-		
+		$style = 'nihilo';
+		if(isset($_COOKIE['_GUI_STYLE'])){
+			$style = $_COOKIE['_GUI_STYLE'];
+		}
+		if(!empty($_GET['style']) && in_array($_GET['style'],array('nihilo','soria','tundra'))){
+			$style = $_GET['style'];
+			setcookie("_GUI_STYLE",$style,time()+365*24*3600,'/');
+		}
 		$dirs = list_dir($path,'dir');
 		$new_dirs[] = array('path'=>$path,'path_encode'=>urlencode($path),'is_writable'=>is_writable($path));
 		if(is_array($dirs)){
@@ -36,6 +48,7 @@ class index {
 		}
 		
 		$tpl->assign('dirs',$new_dirs);
+		$tpl->assign('ui_style',$style);
 	}
 	function is_login(){
 		if(!(isset($_SESSION['login'])&&$_SESSION['login']==1)){
